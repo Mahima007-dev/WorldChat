d7e954850782f25c3c8bc4b7fed4c0f981217df1
mshuklats <mahimas.fullstackdev@gmail.com>
2022-08-12 14:53:50 +0530

    First commit for client side

---
+++ client-side/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+const App = () => {
+  return (
+    <div>
+        <h1>World Chat Application</h1>
+    </div>
+  )
+}
+
+export default App
